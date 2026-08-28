9a390e8ca5dab5f41d5cc860f97cd85d40c2717e
Meng Gao <meng.gao@nasa.gov>
2026-08-28 13:15:23 +0000

    revise render for pdf only

--- render.sh
+++ render.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-quarto render
-touch docs/.nojekyll
+#generate both pdf and html
+#quarto render
 
-#rename pdf with date
+#generate html only
+#already handle by github action
+#quarto render --to html
+#touch docs/.nojekyll
 
-#DATE=$(date +%Y-%m-%d)
 
-#quarto render --to pdf
-
+#generate pdf only
+quarto render --to pdf
 PDF="docs/FastMAPOL-Algorithm-Theoretical-Basis-Document.pdf"
 
+#if need to rename pdf with date
+#DATE=$(date +%Y-%m-%d)
 #if [ -f "$PDF" ]; then
 #    mv "$PDF" "docs/FastMAPOL-ATBD-${DATE}.pdf"
 #fi
 
+#put it into a share dir
 cp $PDF /mnt/mfs/FILESHARE/meng_gao/pace/atbd/
